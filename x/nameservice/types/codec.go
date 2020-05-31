7c47b1dcbce514954cd8c70efb2087044b0c96e2
alyakimenko <urlucidfall@gmail.com>
2020-05-31 11:03:42 +0300

    Register messages

--- x/nameservice/types/codec.go
+++ x/nameservice/types/codec.go
@@ -1,20 +1,16 @@
 package types
 
-import (
-	"github.com/cosmos/cosmos-sdk/codec"
-)
+import "github.com/cosmos/cosmos-sdk/codec"
 
-// RegisterCodec registers concrete types on codec
-func RegisterCodec(cdc *codec.Codec) {
-	// TODO: Register the modules msgs
-}
-
-// ModuleCdc defines the module codec
-var ModuleCdc *codec.Codec
+// ModuleCdc is the codec for the module
+var ModuleCdc = codec.New()
 
 func init() {
-	ModuleCdc = codec.New()
 	RegisterCodec(ModuleCdc)
-	codec.RegisterCrypto(ModuleCdc)
-	ModuleCdc.Seal()
+}
+
+func RegisterCodec(cdc *codec.Codec) {
+	cdc.RegisterConcrete(MsgSetName{}, "nameservice/SetName", nil)
+	cdc.RegisterConcrete(MsgBuyName{}, "nameservice/BuyName", nil)
+	cdc.RegisterConcrete(MsgDeleteName{}, "nameservice/DeleteName", nil)
 }
